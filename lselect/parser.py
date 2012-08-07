823b3a49aae00c4dba2c1186ac756e8ac08a4248
Simon Sapin <simon.sapin@exyr.org>
2012-08-07 19:23:37 +0200

    WIP new parser

--- lselect/parser.py
+++ lselect/parser.py
@@ -11,17 +11,198 @@
 """
 
 from tinycss.tokenizer import tokenize_grouped
+from tinycss.parsing import ParseError
 
 
 def parse_string(string, namespaces=None):
     if isinstance(string, bytes):
         string = string.decode('ascii')
-    return parse(tokenize_grouped(string), namespace)
+    return list(parse(tokenize_grouped(string), namespace))
 
 
 def parse(tokens, namespaces=None):
+    tokens = TokenStream(tokens)
     namespaces = namespaces or {}
-    raise NotImplementedError
+    selectors = []
+    selectors.append(parse_selector(tokens, namespaces))
+    while 1:
+        next = tokens.next()
+        if next.type == 'EOF':
+            return selectors
+        elif next.type == ','
+            selectors.append(parse_selector(tokens, namespaces))
+        else:
+            raise SelectorError('unpexpected %s token.' % next.type)
+
+
+def parse_selector(tokens, namespaces):
+    result, pseudo_element = parse_compound_selector(tokens, namespaces)
+    while 1:
+        has_whitespace = tokens.skip_whitespace()
+        if pseudo_element is not None:
+            return Selector(result, pseudo_element)
+        peek = tokens.peek()
+        if peek.type in ('>', '+', '~'):
+            combinator = peek.type
+        elif has_whitespace:
+            combinator = ' '
+        else:
+            return Selector(result, pseudo_element)
+        compound, pseudo_element = parse_compound_selector(tokens, namespaces)
+        result = CombinedSelector(result, combinator, compound)
+
+
+def parse_compound_selector(tokens, namespaces):
+    simple_selectors = []
+    pseudo_element = None
+
+    tokens.skip_whitespace()
+    namespace, next = parse_namespace(tokens, namespaces)
+    if next.type == 'IDENT':
+        simple_selectors.append(ElementTypeSelector(namespace, next.value))
+        next = tokens.next()
+    elif next.type == '*':
+        simple_selectors.append(UniversalSelector(namespace))
+        next = tokens.next()
+
+    while 1:
+        if next.type == 'HASH':
+            simple_selectors.append(IDSelector(next.value))
+        elif next.type == '.':
+            simple_selectors.append(ClassSelector(get_ident(tokens.next())))
+        elif next.type == '[':
+            simple_selectors.append(parse_attribute_selector(
+                TokenStream(next.content), namespaces))
+        elif next.type == ':':
+            next = tokens.next()
+            if next.type == ':':
+                pseudo_element = get_ident(tokens.next())
+                break
+            elif next.type == 'IDENT':
+                if next.value in ('before', 'after',
+                                  'first-line', 'first-letter'):
+                    pseudo_element = next.value
+                    break
+                else:
+                    simple_selectors.append(PseudoClassSelector(tokens.value))
+            elif next.type == 'FUNCTION':
+                if next.function_name == 'not':
+                    simple_selectors.append(parse_negation(
+                        TokenStream(next.content), namespaces))
+                else:
+                    simple_selectors.append(FunctionalPseudoClassSelector(
+                        next.function_name, next.content))
+        else:
+            break
+        next = tokens.next()
+
+    if simple_selectors:
+        return CompoundSelector(simple_selectors), pseudo_element
+    else:
+        raise SelectorError(
+            next, 'expected a compound selector, got %s' % next.type)
+
+
+def parse_negation(tokens, namespaces):
+    start = tokens.peek()
+    compound, pseudo_element = parse_compound_selector(tokens, namespaces)
+    tokens.skip_whitespace()
+    if (pseudo_element is None and len(compound.simple_selectors) != 1
+            and tokens.peek().type != 'EOF'):
+        return NegationSelector(compound.simple_selectors[0])
+    else:
+        raise SelectorError(start, ':not() only accepts a simple selector')
+
+
+def parse_attribute_selector(tokens, namespaces):
+    tokens.skip_whitespace()
+    namespace, next = parse_namespace(tokens, namespaces, is_attribute=True)
+    name = get_ident(next)
+    skip_whitespace()
+    next = tokens.next()
+    if next.type in ('=', '~', '|', '^', '$', '*'):
+        operator = next.type
+        if next.type != '=' and tokens.peek().type == '=':
+            operator += '='
+            tokens.next()
+        skip_whitespace()
+        next = tokens.next()
+        if next.type not in ('IDENT', 'STRING'):
+            raise SelectorError('unexpected %s token.' % next.type)
+        value = next.value
+    else:
+        operator = value = None
+    skip_whitespace()
+    next = tokens.next().type
+    if next != 'EOF':
+        raise SelectorError('unexpected %s token.' % next)
+    return AttributeSelector(namespace, name, operator, value)
+
+
+def parse_namespace(tokens, namespaces, is_attribute=False):
+    next = tokens.next()
+    if next.type == '|':
+        namespace = None
+        next = tokens.next()
+    elif next.type == '*' and tokens.peek().type == '|':
+        namespace = any
+        tokens.next()
+        next = tokens.next()
+    elif next.type == 'IDENT' and tokens.peek().type == '|' and not (
+            is_attribute and tokens.peek(2).type == '='):
+        prefix = next.value
+        if prefix not in namespaces:
+            raise SelectorError('undefined namespace prefix: ' + prefix)
+        namespace = namespaces[prefix]
+        tokens.next()
+        next = tokens.next()
+    elif is_attribute:
+        namespace = None
+    else:
+        namespace =  namespaces.get(None, any)  # default namespace
+    return namespace, next
+
+
+def get_ident(token):
+    if token.type != 'IDENT':
+        raise SelectorError(token, 'Expected IDENT, got %s' % token.type)
+    return token.value
+
+
+class SelectorError(ParseError):
+    """A specialized ParseError from tinycss for selectors."""
+
+
+class EOFToken(object):
+    type = 'EOF'
+    value = None
+
+
+class TokenStream(object):
+    def __init__(self, tokens):
+        self.tokens = iter(tokens)
+        self.peeked = []
+
+    def peek(self, n=1):
+        while len(self.peeked) < n:
+            self.peeked.insert(0, self.next())
+        return self.peeked[-n]
+
+    def next(self):
+        if self.peeked:
+            return self.peeked.pop()
+        else:
+            token = next(self.tokens, EOFToken)
+            if token.type == 'DELIM':
+                token.type = token.value
+            return token
+
+    def skip_whitespace(self):
+        has_whitespace = False
+        while self.peek().type == 'S':
+            self.next()
+            has_whitespace = True
+        return has_whitespace
 
 
 def ascii_lower(string):
@@ -45,11 +226,11 @@ class Selector(object):
 
 class CombinedSelector(object):
     def __init__(self, left, combinator, right):
-        #: Compound or simple selector
+        #: Combined or compound selector
         self.left = left
         # One of `` `` (a single space), ``>``, ``+`` or ``~``.
         self.combinator = combinator
-        #: Simple selector
+        #: compound selector
         self.right = right
 
     @property
@@ -80,8 +261,8 @@ def ElementTypeSelector(object):
         #: ``any`` (used as a marker) for any namespace (``*`` in CSS)
         #: or a namespace name/URI (not a prefix) as a string.
         #:
-        #: Note that simple type selectors like ``E`` are resolved to
-        #: ``NS|E`` if there is a default namespace ``*|E`` otherwise.
+        #: Note that simple type selectors like ``E`` are resolved to either
+        #: ``NS|E`` or ``*|E``: http://www.w3.org/TR/selectors/#typenmsp
         self.namespace = namespace
         self.element_type = element_type
 
@@ -111,7 +292,9 @@ class ClassSelector(object):
 class AttributeSelector(object):
     specificity =  0, 1, 0
 
-    def __init__(self, name, operator, value):
+    def __init__(self, namespace, name, operator, value):
+        #: Same as :attr:`ElementTypeSelector.namespace`
+        self.namespace = namespace
         self.name = name
         #: A string like ``=`` or ``~=``, or None for ``[attr]`` selectors
         self.operator = operator
