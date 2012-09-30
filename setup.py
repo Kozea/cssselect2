cd23559abfb1b606cabd4545a004a92384bcd0ea
Simon Sapin <simon.sapin@exyr.org>
2012-09-30 22:09:07 +0200

    Update deps.

--- setup.py
+++ setup.py
@@ -21,5 +21,5 @@ setup(
     url='http://packages.python.org/lselect/',
     license='BSD',
     packages=['lselect'],
-    install_requires=['cssselect', 'tinycss', 'lxml']
+    install_requires=['tinycss==0.3', 'lxml']
 )
