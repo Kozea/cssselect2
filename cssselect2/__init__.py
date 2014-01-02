2b29dc85191c300ba11e030d30de16f4dfd79ba9
Simon Sapin <simon.sapin@exyr.org>
2014-01-02 12:48:20 +0000

    Add a Matcher API.

--- cssselect2/__init__.py
+++ cssselect2/__init__.py
@@ -12,6 +12,8 @@
 
 from __future__ import unicode_literals
 
+import operator
+
 from .tree import ElementWrapper
 from .compiler import compile_selector_list, CompiledSelector
 
@@ -20,20 +22,43 @@ VERSION = '0.1a0'
 
 
 class Matcher(object):
-    """
-    :param payload:
-        Any data associated to these selectors,
-        such as :class:`declarations <~tinycss2.ast.Declaration>`
-        parsed from the :attr:`~tinycss2.ast.QualifiedRule.content`
-        of a style rule.
-        Can be any Python object.
-        It will be returned as-is by (TODO)
-    """
     def __init__(self):
         self.selectors = []
+        self.is_sorted = True
 
-    def add_compiled_selectors(self, selectors,
-                               origin='author', important=False):
-        ...
+    def add_selector(self, selector, payload):
+        """
 
-##    def match(self, element, style_attribute_data=None)
+        :param selector:
+            A :class:`CompiledSelector` object.
+        :param payload:
+            Some data associated to the selector,
+            such as :class:`declarations <~tinycss2.ast.Declaration>`
+            parsed from the :attr:`~tinycss2.ast.QualifiedRule.content`
+            of a style rule.
+            It can be any Python object,
+            and will be returned as-is by :meth:`match`.
+
+        """
+        self.is_sorted = False
+        self.selectors.append((selector.test, selector.specificity, payload))
+
+
+    def match(self, element):
+        """
+        Match selectors against the given element.
+
+        :param element:
+            An :class:`ElementWrapper`.
+        :returns:
+            An iterable of the :obj:`payload` objects associated
+            to selectors that match element,
+            in order of highest to lowest :attr:`~CompiledSelector.specificity`.
+
+        """
+        if not self.is_sorted:
+            self.selectors.sort(key=operator.itemgetter(1), reverse=True)
+            self.is_sorted = True
+        for test, _, payload in self.selectors:
+            if test(element):
+                yield payload
