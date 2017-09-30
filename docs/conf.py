0b16ba61f30b1d6df7d547940d76297cd3e27b32
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2017-10-01 00:44:44 +0200

    Add docs

---
+++ docs/conf.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# cssselect2 documentation build configuration file.
+
+import codecs
+import re
+from os import path
+
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+# General information about the project.
+project = 'cssselect2'
+copyright = '2012-2017, Simon Sapin'
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The full version, including alpha/beta/rc tags.
+release = re.search("VERSION = '([^']+)'", codecs.open(
+    path.join(path.dirname(path.dirname(__file__)), 'cssselect2', '__init__.py'),
+    encoding='utf-8',
+).read().strip()).group(1)
+
+# The short X.Y version.
+version = '.'.join(release.split('.')[:2])
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = ['_build']
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'cssselect2doc'
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    ('index', 'cssselect2', 'cssselect2 Documentation',
+     ['Simon Sapin'], 1)
+]
+
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+  ('index', 'cssselect2', 'cssselect2 Documentation',
+   'Simon Sapin', 'cssselect2', 'One line description of project.',
+   'Miscellaneous'),
+]
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {
+    'py2': ('http://docs.python.org/2', None),
+    'py3': ('http://docs.python.org/3', None),
+    'webencodings': ('http://pythonhosted.org/webencodings/', None)}
---
+++ docs/index.rst
@@ -0,0 +1,43 @@
+.. include:: ../README.rst
+
+
+Installation
+============
+
+Installing cssselect2 with pip_ should Just Work::
+
+    pip install cssselect2
+
+This will also automatically install cssselect2’s only dependency, tinycss2_.
+cssselect2 and tinycss2 both only contain Python code and should work on any
+Python implementation, although they’re only tested on CPython.
+
+.. _pip: http://pip-installer.org/
+.. _tinycss2: http://tinycss2.readthedocs.io/
+
+
+Basic Example
+=============
+
+Here is a classical cssselect2 workflow:
+
+- parse a CSS stylesheet using tinycss2_,
+- store the CSS rules in a :meth:`~cssselect2.Matcher` object,
+- parse an HTML document using an ElementTree-like parser,
+- wrap the HTML tree in a :meth:`~cssselect2.ElementWrapper` object,
+- find the CSS rules matching each HTML tag, using the matcher and the wrapper.
+
+.. include:: ../example.py
+
+.. module:: tinycss2
+.. autofunction:: Matcher
+.. autofunction:: compile_selector_list
+.. autofunction:: CompiledSelector
+.. autofunction:: ElementWrapper
+.. autofunction:: SelectorError
+
+
+Changelog
+=========
+
+.. include:: ../CHANGES
